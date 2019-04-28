fd1e02bae61ae92d39fe97a55c4ed7e6ff6f09ba
John Colagioia <john@colagioia.net>
2019-04-28 06:59:33 -0400

    Stub out UI using the qmlrs factorial example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+#[macro_use]
+extern crate qmlrs;
+
+struct Factorial;
+impl Factorial {
+    fn calculate(&self, x: i64) -> i64 {
+        (1 ..= x).fold(1, |t,c| t * c)
+    }
+}
+
+Q_OBJECT! { Factorial:
+    slot fn calculate(i64);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut engine = qmlrs::Engine::new();
+
+    engine.set_property("factorial", Factorial);
+    engine.load_local_file("src/neatoui.qml");
+
+    engine.exec();
 }
